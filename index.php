175463a70bfa9331dccde461f6796c45229c7548
JohnHenry <johnhenry514@gmail.com>
2019-05-12 19:20:21 -0700

    began to implement the edit feature, adding forms for edit, converted tables into 1 large table, teamName as primary key and divison column to group them

--- index.php
+++ index.php
@@ -20,6 +20,48 @@
 
             //connect to database
             $link = connectToDatabase();
+
+            //update
+            if(!empty($_GET['updateTeam'])){
+                $updateTeam = $_GET['updateTeam'];
+
+                $sql = 'SELECT teamName, wins, losses, winPerc, gamesBack, lastTen, streak FROM alwest WHERE teamName = "$updateTeam"';
+
+                ?>
+                <div class = 'editPopup'>
+                        <h2>Updating team: <?php echo $updateTeam ?></h2>
+                        <form>
+                            <label>Wins:</label>
+                            <input name = 'wins' type = 'text' value = <?php $wins ?>autofocus> <br>
+                            <label>Losses:</label>
+                            <input name = 'losses' type = 'text'> <br>
+                            <label>Win%:</label>
+                            <input name = 'winPerc' type = 'text'> <br>
+                            <label>GB:</label>
+                            <input name = 'gamesBack' type = 'text'> <br>
+                            <label>L10:</label>
+                            <input name = 'lastTen' type = 'text'> <br>
+                            <label>Streak:</label>
+                            <input name = 'streak' type = 'text'> <br>
+                            <input type="submit" value="sendUpdate">
+                        </form>
+                </div>
+                <?php
+            }
+
+            
+
+            if(!empty($_GET['sendUpdate'])){
+                //get all update fields
+                $wins = $_GET['wins'];
+                $losses = $_GET['losses'];
+                $winPerc = $_GET['winPerc'];
+                $gamesBack = $_GET['gamesBack'];
+                $lastTen = $_GET['lastTen'];
+                $streak = $_GET['streak'];
+                //$sql = "UPDATE tblName
+                        //SET wins = $wins, losses = $losse"
+            }
         ?>
         <div class="ALNL">
             <h4 class = "ALHeader">American League West</h4>
@@ -35,14 +77,14 @@
                 </tr>
                 <?php
                     //select the data from the table ALWest
-                    $sql = 'SELECT teamName, wins, losses, winPerc, gamesBack, lastTen, streak FROM alwest ORDER BY winPerc DESC';
+                    $sql = "SELECT * FROM mlbstandings WHERE division = 'alwest' ORDER BY winPerc DESC";
 
                     //results gives an array of containing query results
                     $result = mysqli_query($link, $sql) or die('SQL syntax error: '.mysqli_error($link));
 
                     while($row = mysqli_fetch_array($result)){
                         echo "<tr>
-                                <td class = 'team'>$row[teamName]</td>
+                                <td class = 'team'><a href='?updateTeam=$row[teamName]'>$row[teamName]</a></td>
                                 <td>$row[wins]</td>
                                 <td>$row[losses]</td>
                                 <td>$row[winPerc]</td>
@@ -68,7 +110,7 @@
                 </tr>
                 <?php
                     //select the data from the table ALWest
-                    $sql = 'SELECT teamName, wins, losses, winPerc, gamesBack, lastTen, streak FROM alcentral ORDER BY winPerc DESC';
+                    $sql = "SELECT * FROM mlbstandings WHERE division = 'alcentral' ORDER BY winPerc DESC";
 
                     //results gives an array of containing query results
                     $result = mysqli_query($link, $sql) or die('SQL syntax error: '.mysqli_error($link));
@@ -101,8 +143,8 @@
                 </tr>
                 <?php
                     //select the data from the table ALEast
-                    $sql = 'SELECT teamName, wins, losses, winPerc, gamesBack, lastTen, streak
-                    FROM aleast ORDER BY winPerc DESC';
+                    $sql = "SELECT * FROM mlbstandings WHERE division = 'aleast' ORDER BY winPerc DESC";
+
 
                     //results gives an array of containing query results
                     $result = mysqli_query($link, $sql) or die('SQL syntax error: '.mysqli_error($link));
@@ -135,8 +177,8 @@
                 </tr>
                 <?php
                     //select the data from the table ALWest
-                    $sql = 'SELECT teamName, wins, losses, winPerc, gamesBack, lastTen, streak
-                    FROM nlwest ORDER BY winPerc DESC';
+                    $sql = "SELECT * FROM mlbstandings WHERE division = 'nlwest' ORDER BY winPerc DESC";
+
 
                     //results gives an array of containing query results
                     $result = mysqli_query($link, $sql) or die('SQL syntax error: '.mysqli_error($link));
@@ -156,7 +198,7 @@
             </table>
         </div>
         <div class="ALNL">
-            <h4 class = "NLHeader">Natinoal League Central</h4>
+            <h4 class = "NLHeader">National League Central</h4>
             <table class="Division">
                 <tr>
                     <th></th> <!--Blank header-->
@@ -169,8 +211,8 @@
                 </tr>
                 <?php
                     //select the data from the table ALWest
-                    $sql = 'SELECT teamName, wins, losses, winPerc, gamesBack, lastTen, streak
-                    FROM nlcentral ORDER BY winPerc DESC';
+                    $sql = "SELECT * FROM mlbstandings WHERE division = 'nlcentral' ORDER BY winPerc DESC";
+
 
                     //results gives an array of containing query results
                     $result = mysqli_query($link, $sql) or die('SQL syntax error: '.mysqli_error($link));
@@ -203,8 +245,8 @@
                 </tr>
                 <?php
                     //select the data from the table ALWest
-                    $sql = 'SELECT teamName, wins, losses, winPerc, gamesBack, lastTen, streak
-                    FROM nleast ORDER BY winPerc DESC';
+                    $sql = "SELECT * FROM mlbstandings WHERE division = 'nleast' ORDER BY winPerc DESC";
+
 
                     //results gives an array of containing query results
                     $result = mysqli_query($link, $sql) or die('SQL syntax error: '.mysqli_error($link));
